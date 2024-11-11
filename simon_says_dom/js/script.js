100b5c1ab0b38bdbf00307d1ffb2e059a9b1bc40
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-11 19:23:37 +0100

    fixed e funzionante

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -43,22 +43,21 @@ btn.addEventListener("click", inserimento);
 const btnRisposte = document.querySelector(".risposte")
 
 function checker(){
-    answerForm.classList.add("d-none");
+    btnRisposte.classList.add("d-none");
     const n1 = parseInt(document.getElementById("n1").value);
     const n2 = parseInt(document.getElementById("n2").value);
     const n3 = parseInt(document.getElementById("n3").value);
     const n4 = parseInt(document.getElementById("n4").value);
     const n5 = parseInt(document.getElementById("n5").value);
+    let somma = 0;
     const numeriRisultati = [];
-    const somma = 0
     const numeriGuessed = [n1,n2,n3,n4,n5];
     const message = document.getElementById("message")
-    for( let i = 0; i< numeriGuessed;i++){
-        for( let j = 0; j< numeriEstratti; j++){
+    for( let i = 0; i< numeriGuessed.length;i++){
+        for( let j = 0; j< numeriEstratti.length; j++){
             if (numeriGuessed[i] === numeriEstratti[j]){
-                console.log("ciao")
+                somma = somma + 1;
                 numeriRisultati.push(numeriGuessed[i]);
-                somma+=1;
             }
         }
     }
